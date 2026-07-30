51e433cb9b278a48cc34b8959420d1e968761a8b
Gokulstark62 <gokulranjan01222006@gmail.com>
2026-07-30 23:10:57 +0530

    Time: 0 ms (100%), Space: 42.5 MB (67.71%) - LeetHub

---
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+        int ele = k / (n - 1);
+        int mod = k % (n - 1);
+        return ele % 2 == 1 ? n - 1 - mod : mod;
+    }
+}
